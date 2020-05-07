68b9a93b7f8466cdeab7d80be97bc1af70464f59
Michael Romay <amerror@gmail.com>
2020-05-06 20:40:13 -0700

    css for light v dark theme

--- src/components/Paging/Paging.jsx
+++ src/components/Paging/Paging.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { usePage, useHandlePage, useCharacters } from '../../hooks/CharacterProvider';
+import { usePage, useHandlePage, useCharacters, useTheme } from '../../hooks/CharacterProvider';
+import stylesLight from './pagingLight.css';
+import stylesDark from './pagingDark.css';
 
 const Paging = () => {
   const page = usePage();
   const handlePage = useHandlePage();
   const characters = useCharacters();
+  const theme = useTheme();
+  const styles = theme === 'light' ? stylesLight : stylesDark;
 
   return (
-    <section>
-      <button onClick={() => handlePage(-1)} disabled={page === 1}>&#8678</button>
-        <button onClick={() => handlePage(1)} disabled={characters.length < 20}>&#8680</button>
+    <section className={styles.Paging}>
+      <button onClick={() => handlePage(-1)} disabled={page === 1}>&#8678;</button>
+        <button onClick={() => handlePage(1)} disabled={characters.length < 20}>&#8680;</button>
     </section>
   );
 };
--- src/container/CharacterList/CharacterList.jsx
+++ src/container/CharacterList/CharacterList.jsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import List from '../../components/List/List';
-import { useCharacters } from '../../hooks/CharacterProvider';
+import { useCharacters, useTheme } from '../../hooks/CharacterProvider';
+import stylesLight from './characterListLight.css';
+import stylesDark from './characterListDark.css';
 
 const CharacterList = () => {
   const characters = useCharacters();
+  const theme = useTheme();
+  const styles = theme === 'light' ? stylesLight : stylesDark;
 
   const characterNodes = characters.map(character => {
     return <List key={character.id} {...character} />;
   });
   
   return (
-    <>
+    <section className={styles.CharacterList}>
       {characterNodes}
-    </>
+    </section>
   );
 };
 
